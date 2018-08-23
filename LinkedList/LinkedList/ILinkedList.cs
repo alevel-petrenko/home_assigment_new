8822f524a97eb1c05919f7c5992fe5ec7baafb51
Anton Petrenko <petrenko.anton22@gmail.com>
2018-08-23 16:57:56 +0300

    initial commit with all files

---
+++ LinkedList/LinkedList/ILinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+        public interface ILinkedList
+        {
+            Node Head { get; set; }
+
+            Node Tail { get; set; }
+
+            void Add(Node node);
+
+            void Remove(int value); //in case if there are 2 items with this value remove first
+
+            void OutputAllNodes();
+        }
+
+        public class Node
+        {
+            public Node Next { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        //реализовать интерфейс для добавления эль-то в список
+        //и вывод на экран всех значений которые были добавлены.
+    }
