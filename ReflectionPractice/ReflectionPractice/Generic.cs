cf1c52c49d328db21853bedf9a546e2b02b152cc
Anton Petrenko <petrenko.anton22@gmail.com>
2018-08-24 23:22:05 +0300

    added reflection class work

---
+++ ReflectionPractice/ReflectionPractice/Generic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPractice
+{
+    class Generic
+    {
+        public bool AreSame<T> (T operand1, T operand2) where T : class //class - ref type, struct - value type, new() - instances of classes, Person конкретный персон и его наследники
+        {            
+            return operand1.Equals(operand2);
+        }
+
+        public void ShowInfo<T>(T operand)
+        {
+            if (operand!=null)
+            {
+                foreach (var prop in operand.GetType().GetProperties())
+                {
+                    Console.WriteLine($"PropertyName : {prop.Name}," +
+                        $"propertyType : {prop.PropertyType.Name}," +
+                        $"Property Value : {prop.GetValue(operand)}");
+                    if (prop.PropertyType.IsPrimitive || prop.PropertyType.IsEnum
+                        || prop.PropertyType == typeof(string) || prop.PropertyType == typeof(DateTime))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var propValue = prop.GetValue(operand, null);
+                        ShowInfo(propValue);
+                    }
+                }
+            } 
+            
+
+        }
+    }
+}
---
+++ ReflectionPractice/ReflectionPractice/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPractice
+{
+    class Program
+    {
+        ///рекурсивно зайти в персон и достучаться до CardName, CardNumber и вывести на экран
+
+        static void Main(string[] args)
+        {
+            Generic gen = new Generic();
+            Console.WriteLine(gen.AreSame<string>("Olya", "Sasha"));
+
+
+            Calc calc = new Calc();
+            Type calcType = calc.GetType();
+
+            MethodInfo method = calcType.GetMethod("Add");
+            var param = new object[] { 6, 7 }; //create params that will be passed to method
+            //массив object обязательно
+
+            Console.WriteLine(method?.Invoke(calc, param));
+            //? проверка на налл
+
+            Person p = new Person()
+            {
+                Age = 25,
+                CardInfo = new BankCardInfo()
+                {
+                    CardName = "Name",
+                    CardNumber = "2233 5566 4488 7788"
+                },
+                Email = "dfdjfo@gmail",
+                FirstName = "Kolya",
+                LastName = "Bomzh"
+            };
+
+            foreach (var prop in p.GetType().GetProperties())
+            {
+                Console.WriteLine($"PropertyName : {prop.Name}," +
+                    $"propertyType : {prop.PropertyType.Name}," +
+                    $"Property Value : {prop.GetValue(p)}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public BankCardInfo CardInfo { get; set; }
+        [SecureData] //action filter
+        public string Password { get; set; }
+    }
+
+    public class BankCardInfo
+    {
+        [SecureData] //action filter
+        public string Cvv { get; set; }
+        public string Pin { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+    }
+
+     public class SecureData : Attribute
+    {
+
+    }
+}
