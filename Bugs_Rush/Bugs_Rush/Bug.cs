17267bf21c83c86dd489780ae89fb08bd1f2a252
Anton Petrenko <petrenko.anton22@gmail.com>
2018-09-05 00:13:27 +0300

    bug rush

---
+++ Bugs_Rush/Bugs_Rush/Bug.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bugs_Rush
+{
+    class Bug
+    {
+        public static object obj = new object();
+        private int _xPos;
+        private int _yPos;
+        private readonly char _symbol;
+        private Random rnd = new Random();
+
+        public Bug(char symbol, int x, int y)
+        {
+            _symbol = symbol;
+            _xPos = x;
+            _yPos = UI.lineY = y;
+        }
+
+        public void Move()
+        {
+            int i = 0;
+            while (i<10)
+            {
+                Thread.Sleep(rnd.Next(500, 1000));
+                Step();
+                lock (obj)
+                {
+                    Console.SetCursorPosition(_yPos, _xPos);
+                    Console.Write(_symbol);
+                    ClearPrint();
+                }
+            }
+            i++;
+        }
+
+        private void Step ()
+        {
+            _yPos++;
+        }
+
+        private void ClearPrint ()
+        {
+            Console.SetCursorPosition(_yPos - 1, _xPos);
+            Console.Write(" ");
+        }
+    }
+}
---
+++ Bugs_Rush/Bugs_Rush/UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugs_Rush
+{
+    static class UI
+    {
+        public static int lineY;
+        public static void FinishLine ()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.SetCursorPosition(lineY+i, 1);
+                Console.Write('|');
+            }
+        }
+    }
+}
