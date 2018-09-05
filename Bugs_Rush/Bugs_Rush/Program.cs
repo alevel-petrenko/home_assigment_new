16c268a32ff8363beb279017f1deaefab334c07b
Anton Petrenko <petrenko.anton22@gmail.com>
2018-09-05 08:05:47 +0300

    added project about bugs

---
+++ Bugs_Rush/Bugs_Rush/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugs_Rush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            Task t1 = new Task(() =>
+            {
+                Bug bug1 = new Bug('&', 1, 1);
+                bug1.Move();
+            });
+
+            Task t2 = new Task(() =>
+            {
+                Bug bug2 = new Bug('$', 2, 1);
+                bug2.Move();
+            });
+
+            Task t3 = new Task(() =>
+            {
+                Bug bug3 = new Bug('#', 3, 1);
+                bug3.Move();
+            });
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bugs_Rush/Bugs_Rush/UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugs_Rush
+{
+    static class UI
+    {
+        static int lineX;
+        static void FinishLine ()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.SetCursorPosition(lineX+i, 1);
+                Console.Write('|');
+            }
+        }
+    }
+}
