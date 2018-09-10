70885eec01c4647222a4d679766367bdb6da2800
Anton Petrenko <petrenko.anton22@gmail.com>
2018-09-10 22:05:21 +0300

    first app

---
+++ Lesson1/Asp-Preview/Asp-Preview/Models/SummModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Preview.Models
+{
+    public class SummModel
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Result { get; set; }
+    }
+}
---
+++ Square/Square_Logic/Services/CalculateService.cs
@@ -0,0 +1,27 @@
+﻿using Square_Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Logic.Services
+{
+    public interface ICalculateService
+    {
+        void Calculate(Inputs model);
+    }
+
+    //сделать дома правильную реализацию CalculateService
+
+    public class CalculateServiceMoke : ICalculateService
+    {
+        public void Calculate(Inputs model)
+        {
+            model.X = new List<double>
+            {
+                5, -5 //Mocked
+            };
+        }
+    }
+}
