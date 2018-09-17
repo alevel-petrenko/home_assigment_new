3f867b47da1d6b411e33de2f79a0b6e2ddc0320e
Anton Petrenko <petrenko.anton22@gmail.com>
2018-09-17 14:18:23 +0300

    adding project Phonebook in the new branch Option "Details" is shown all telephones from list

---
+++ PhoneBook/PhoneBook/Logic/Models/Phone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class Phone
+    {
+        public PhoneTypes Type { get; set; }  //1 home, 2 mobile, 3 work
+
+        public string Number { get; set; }
+    }
+
+    public enum PhoneTypes
+    {
+        Home=1,
+        Mobile,
+        Work
+    }
+}
