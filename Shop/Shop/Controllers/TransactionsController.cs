61707dcde1a861592ef9998ec54fb3ee6e6e1437
alevel-petrenko <petrenko.anton22@gmail.com>
2018-12-12 01:06:38 +0200

    Transaction controller without ninject

--- Shop/Shop/Controllers/TransactionsController.cs
+++ Shop/Shop/Controllers/TransactionsController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BussinesLogic.Extensions;
+using BussinesLogic.Service;
+using ShopData.DTO__BusinessModels_;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +12,46 @@ namespace Shop.Controllers
 {
     public class TransactionsController : ApiController
     {
+        private readonly ITransactionService _transactionService = new TransactionService();
 
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var _transactionsList = _transactionService.GetAll().Select(b => b.ToViewModel());
+
+            return Ok(_transactionsList);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var client = _transactionService.Get(id).ToViewModel();
+
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody] TransactionViewModel model)
+        {
+            int clientId = _transactionService.Add(model.ToDTOModel());
+
+            return Ok(clientId);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update([FromBody] TransactionViewModel model)
+        {
+            _transactionService.Update(model.ToDTOModel());
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            _transactionService.Delete(id);
+
+            return Ok();
+        }
     }
 }
